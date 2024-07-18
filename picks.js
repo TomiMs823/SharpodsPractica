2a7e81357645e8cf42b6fa0cbe2a624d00192105
TomiMs823 <tms232005@gmail.com>
2024-07-18 17:42:17 -0500

    Cambié el lenguaje de Python a Java Script y le añadí el CSS y el HTML sin ningún contenido aún

---
+++ picks.js
@@ -0,0 +1,4 @@
+console.log("A Santi le gusta mi polla")
+console.log("Estiven es millonario")
+console.log("Caddiefor")
+console.log("Mi mamá me mima")
--- picks.py
+++
@@ -1,4 +0,0 @@
-print ("A Santi le gusta mi polla")
-print ("Estiven es millonario")
-print ("Caddiefor")
-print ("Mi mamá me mima")
